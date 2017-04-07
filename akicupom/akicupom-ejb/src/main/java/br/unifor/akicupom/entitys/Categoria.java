9fc55fa56ed4438ef572c639ebb353623bb0c69b
rootcoder <rootcoder@rootcoder>
2017-04-07 11:21:33 -0300

    Criei a Entity Categoria e alterei o a entity cupom colocando a Categoria como Enumerated

--- akicupom/akicupom-ejb/src/main/java/br/unifor/akicupom/entitys/Categoria.java
+++ akicupom/akicupom-ejb/src/main/java/br/unifor/akicupom/entitys/Categoria.java
@@ -1,5 +1,18 @@
 package br.unifor.akicupom.entitys;
 
-public class Categoria {
 
+public enum Categoria {
+	
+	GASTRONOMIA("Gastronomia"), DIVERSAO("Diversao"), SERVICO("Servicos"), 
+	VIAGEM("Viagem"), EVENTO("Evento"), SAUDE("Saude"), SUPERMERCADO("Supermercado");
+	
+	private String nome;
+	
+	Categoria(String nome){
+		this.nome = nome;
+	}
+	
+	public String getNome(){
+		return this.nome;		
+	}
 }
--- akicupom/akicupom-ejb/src/main/java/br/unifor/akicupom/entitys/Cupom.java
+++ akicupom/akicupom-ejb/src/main/java/br/unifor/akicupom/entitys/Cupom.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -26,6 +28,7 @@ public class Cupom implements Serializable{
 	private String descricao;
 	
 	@Column(nullable = false)
+	@Enumerated(EnumType.ORDINAL)
 	private Categoria cat;
 	
 	@Column(nullable = false)
@@ -133,3 +136,5 @@ public class Cupom implements Serializable{
 	}
 	
 }
+
+
